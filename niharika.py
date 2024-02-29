46f81a3ccef6adba95d0ca0eab31733f213bdffc
username7227 <95582745+username7227@users.noreply.github.com>
2024-02-29 13:17:09 +0530

    Add files via upload

---
+++ niharika.py
@@ -0,0 +1,12 @@
+def greet(name):
+    """Function to greet a person"""
+    return f"Hey {name}"
+
+def main():
+    """Main function"""
+    name = "Niharika"
+    message = greet(name)
+    print(message)
+
+if __name__ == "__main__":
+    main()
